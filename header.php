71279002ce840b7bd1533c5cad8c8f176ab57a3e
Oscar Rojas <rogmz58@gmail.com>
2023-07-18 09:51:13 -0400

    creacion cabecera php

---
+++ header.php
@@ -0,0 +1,53 @@
+<link rel="stylesheet" href="css/sidebar.css" type="text/css" />		
+        <link rel="stylesheet" href="css/rightSidebar.css" type="text/css" />	
+        <link rel="stylesheet" href="css/azul_marino.css" type="text/css" />
+        <link rel="stylesheet" href="css/jquery.ui.tabs.css" type="text/css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Belanosima&display=swap" rel="stylesheet">
+
+        <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700&display=swap" rel="stylesheet">
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="css/common.css" type="text/css" />
+  
+  
+  <img src="img/pageHeaderTitleImage_es_ES.jpg" style="width: 100%"  alt="Encabezado de página" />
+    <nav class="navbar navbar-expand-lg" style="background-color:#0550d3;">
+    
+      <div class="container-fluid" >
+        
+        <a class="navbar-brand" style="color:white; font-family: 'Belanosima', sans-serif;text-decoration: none;">Ñande Cine</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#" style="color:white;font-family: 'Noto Sans TC', sans-serif;font-size:18px">Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#" style="color:white;font-family: 'Noto Sans TC', sans-serif;font-size:18px">Noticias</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#" style="color:white;font-family: 'Noto Sans TC', sans-serif;font-size:18px">Peliculas</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link"  href="#" style="color:white;font-family: 'Noto Sans TC', sans-serif;font-size:18px">Reseñas</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link "  href="#" style="color:white;font-family: 'Noto Sans TC', sans-serif;font-size:18px">Nosotros</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" style="color:white;font-family: 'Noto Sans TC', sans-serif;font-size:18px" href="#">Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav> 
+
+ 
